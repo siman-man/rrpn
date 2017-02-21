4315137c8af6fdc13b86f910ce4f4e8ba4081861
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-02-21 19:09:58 +0900

    update example

--- examples/sample.rb
+++ examples/sample.rb
@@ -3,21 +3,26 @@ require 'rrpn'
 rpn = '1 + 2 + 3'.to_rpn
 p rpn.queue #=> [1, 2, :+, 3, :+]
 p rpn.calc  #=> 6
+puts rpn
 
 rpn = '(1.0 + 2.0) * 3'.to_rpn
 p rpn.queue #=> [1.0, 2.0, :+, 3, :*]
 p rpn.calc  #=> 9.0
+puts rpn
 
 rpn = '1/2r + 1/3r'.to_rpn
 p rpn.queue #=> [1, "2r", :/, 1, "3r", :/, :+]
 p rpn.calc  #=> (5/6)
+puts rpn
 
 rpn = '(1+2i) + (3+3i)'.to_rpn
 p rpn.queue #=> [1, 3, :<<, 16, 2, :>>, :+]
 p rpn.calc  #=> (4+5i)
+puts rpn
 
 rpn = '(1 << 3) + (16 >> 2)'.to_rpn
 p rpn.queue #=> [1, (0+2i), :+, 3, (0+3i), :+, :+]
 p rpn.calc  #=> 12
+puts rpn
 
 p RRPN.calc([3, 4, :*]) #=> 12
